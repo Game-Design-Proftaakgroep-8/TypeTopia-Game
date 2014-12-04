9d5e0d449cb08f233f73861c263394c4fd58ce36
bartbouten <bart.bouten@student.fontys.nl>
2014-12-04 15:56:34 +0100

    FileHandler Voorbeeld

---
+++ Proof of concept Technische onderdelen/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+
+	private FileHandler file;
+	public Text countText;
+
+	private float blockCount;
+
+	// Use this for initialization
+	void Start () {
+		file = GetComponent<FileHandler> ();
+		file.Load ();
+
+		blockCount = file.GetBlockCount ();
+
+		UpdateText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Increase(){
+		blockCount += 1;
+		UpdateText ();
+	}
+
+	public void Decrease(){
+		blockCount -= 1;
+		UpdateText ();
+	}
+
+	public void Ok(){
+		file.SetBlockCount (blockCount);
+		file.Save ();
+	}
+
+	private void UpdateText(){
+		countText.text = "BlockCount: " + blockCount;
+	}
+}
--- Proof of concept Technische onderdelen/Assets/Scripts/FileHandler.cs
+++ Proof of concept Technische onderdelen/Assets/Scripts/FileHandler.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class FileHandler : MonoBehaviour {
 
+
+	private float blockCount;
+
 	// Use this for initialization
 	void Start () {
-	
+		blockCount = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	public float GetBlockCount(){
+		return blockCount;
+	}
+
+	public void SetBlockCount(float count){
+		this.blockCount = count;
+	}
+
+
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/testInfo.dat");
+		PlayerData data = new PlayerData ();
+
+		data.blockCount = blockCount;
+
+		bf.Serialize (file, data);
+		file.Close ();
+	}
+
+	public void Load(){
+		if(File.Exists(Application.persistentDataPath + "/testInfo.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open (Application.persistentDataPath + "/testInfo.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file);
+			file.Close ();
+
+			blockCount = data.blockCount;
+		}
+	}
+}
+
+[Serializable]
+class PlayerData {
+	public float blockCount;
 }
