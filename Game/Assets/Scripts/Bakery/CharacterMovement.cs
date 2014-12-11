797cb74e51ed90cfbb922644d7439ff95784e917
xChocoMel <melaniekwetters@hotmail.com>
2014-12-11 12:43:58 +0100

    sorry

---
+++ Game/Assets/Scripts/Bakery/CharacterMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	
+	private GameObject hit; 
+	private Touch touch;
+
+	private float countDown;
+
+	// Use this for initialization
+	void Start ()
+	{
+		hit = null;
+		countDown = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (countDown != 0f)
+		{
+			if (Time.time > countDown + 1f)
+			{
+				LevelManager.StartGame(hit);
+			}
+		}
+		else
+		{
+			//Touch input
+			if(Input.touchCount == 1)
+			{
+				touch = Input.touches[0];
+				GameObject o = InputDetection.CheckTouch(touch.position);
+
+				if (o != null)
+				{
+					hit = o;
+				}
+			}
+
+			if (hit != null)
+			{
+				LevelManager.LerpCharacter(hit);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		countDown = Time.time;
+	}
+}
---
+++ Game/Assets/Scripts/Bakery/LevelManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject oven;
+	public GameObject counter;
+	public GameObject workbench;
+
+	private string ovenScene;
+	private string counterScene;
+	private string workbenchScene;
+
+	private int nextCustomerNr;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ovenScene = "Oven";
+		counterScene = "Counter";
+		workbenchScene = "Workbench";
+
+		nextCustomerNr = 1;		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public int GetCostumerNr()
+	{
+		int nr = this.nextCustomerNr;
+		this.nextCustomerNr++;
+		return nr;
+	}
+
+	public void StartGame(GameObject hit)
+	{
+		//start game
+		if (hit == oven)
+		{
+			print("Start game oven");
+			Application.LoadLevel(ovenScene);
+		}
+		else if (hit == counter)
+		{
+			print("Start game counter");
+			Application.LoadLevel(counterScene);
+		}
+		else if (hit == workbench)
+		{
+			print("Start game workbench");
+			Application.LoadLevel(workbenchScene);
+		}
+	}
+
+	public void LerpCharacter(GameObject touchObject)
+	{
+		Vector2 currentPos = this.transform.position;
+		Vector2 endPos = touchObject.transform.position;
+		
+		this.transform.position = Vector2.Lerp (currentPos, endPos, 0.5f * Time.fixedDeltaTime);
+	}
+}
