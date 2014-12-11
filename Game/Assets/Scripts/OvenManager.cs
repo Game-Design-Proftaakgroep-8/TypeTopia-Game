5727a8aebcfb84ed2a929b6696ee5a1d1d3ed20a
bartbouten <bart.bouten@student.fontys.nl>
2014-12-11 11:40:40 +0100

    Oven shit

---
+++ Game/Assets/Scripts/OvenManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OvenManager : MonoBehaviour {
+
+	public Text timer;
+	public GameObject ClockNowH;
+	public GameObject ClockNowM;
+	public GameObject ClockThenH;
+	public GameObject ClockThenM;
+
+
+	private float timeNowH;
+	private float timeNowM;
+	private float timeThenH;
+	private float timeThenM;
+	private float timeNeeded;
+	private float timeTimer;
+
+	// Use this for initialization
+	void Start () {
+		timeNowH = 6;
+		timeNowM = 40;
+
+		timeThenH = 6;
+		timeThenM = 20;
+
+		timeNeeded = 45;
+
+		timeTimer = 0;
+
+		updateTimer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setTimeNow() {
+		ClockNowH.transform.Rotate (Vector3.forward * (timeNowH * -30));
+		ClockNowM.transform.Rotate (Vector3.forward * ((timeNowM/5) * -30));
+	}
+
+	void setTimeThen() {
+
+	}
+
+	private void updateTimer() {
+		if(timeTimer < 10)
+			timer.text = "0";
+		else
+			timer.text = "";
+
+		timer.text += timeTimer + " min";
+	}
+
+	public void increaseTimerTime() {
+		timeTimer += 5;
+		updateTimer ();
+	}
+
+	public void decreaseTimerTime() {
+		if(timeTimer > 0) {
+			timeTimer -= 5;
+			updateTimer ();
+		}
+	}
+
+}
