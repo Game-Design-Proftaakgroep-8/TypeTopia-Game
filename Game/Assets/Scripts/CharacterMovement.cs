a7cf68f77663703893d46a3a344c52c1c5f85e80
xChocoMel <melaniekwetters@hotmail.com>
2014-12-11 10:45:43 +0100

    sync

--- Game/Assets/Scripts/CharacterMovement.cs
+++ Game/Assets/Scripts/CharacterMovement.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 
+	public CharacterController controller;
+	public GameObject oven;
+	public GameObject counter;
+	public GameObject workbench;
+
+	private int count = 0;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (count == 0)
+		{
+			count = 1;
+			Touch touch = Input.GetTouch(1);
+			GetTouchPosition(touch);
+			count = 0;
+		}
+	}
+
+	public void GetTouchPosition(Touch touch)
+	{
+		Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+		Vector2 touchPos = new Vector2(position.x, position.y);
+		RaycastHit2D hit = Physics2D.Raycast(touchPos, Vector2.zero);
+
+		if(hit)
+		{                                
+			GameObject tObject = GameObject.Find(hit.transform.gameObject.name);	
+			Vector2 currentPos = this.transform.position;
+			Vector2 endPos = tObject.transform.position;
+			this.transform.position = Vector2.Lerp (currentPos, endPos, 1.0f * Time.fixedDeltaTime);
+		}
 	}
 }
