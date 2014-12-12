ea9d1717b91cc1fef45286fdf18be5359b429859
Alexandervc <a.vancuijk@student.fontys.nl>
2014-12-12 11:31:37 +0100

    Vervolg maatbeker verschillende eenheden etc

--- Game/Assets/Scripts/Workbench/AssignmentManager.cs
+++ Game/Assets/Scripts/Workbench/AssignmentManager.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 public class AssignmentManager : MonoBehaviour {
 	public WaterController waterController;
+	public Text winLoseText;
+	public Text recipeText;
 	
 	private Recipe recipe;
 	private int wanted;
 	private int marge;
+	private RecipeRow currentIngredient;
+	private int currentAnswer;
 
 	// Use this for initialization
 	void Start () {
@@ -15,10 +21,14 @@ public class AssignmentManager : MonoBehaviour {
 		string product = "Brood";
 		int amount = 4;
 		recipe = new Recipe (product, amount);
-		recipe.AddRecipeRow (Ingredients.Bloem, 2, true);
-		recipe.AddRecipeRow (Ingredients.Water, 800, false);
+		recipe.AddRecipeRow (Ingredients.Bloem, 2, true, UnitPrexixes.k, "g");
+		recipe.AddRecipeRow (Ingredients.Water, 800, false, UnitPrexixes.m, "l");
+		recipe.AddRecipeRow (Ingredients.Water, 0.4f, false, UnitPrexixes._, "l");
+		//recipe.AddRecipeRow (Ingredients.Water, 800, false, UnitPrexixes.m, "l");
 		wanted = 3;
 		marge = 25;
+		recipeText.text = recipe.ToString();
+		StartCoroutine (this.startFirstGame());
 	}
 	
 	// Update is called once per frame
@@ -26,24 +36,52 @@ public class AssignmentManager : MonoBehaviour {
 	
 	}
 
-	public void CheckAssignment() {
-		RecipeRow ingredient = recipe.GetNext ();
-		if (ingredient != null) {
-			int answer = ingredient.amount / recipe.amount * wanted;
-			switch(ingredient.ingredient) {
+	private void NextGame() {
+		currentIngredient = recipe.GetNext();
+		if (currentIngredient != null) {
+			currentAnswer = Convert.ToInt32((currentIngredient.amount * (int)currentIngredient.unitPrefix) / recipe.amount * wanted);
+			switch(currentIngredient.ingredient) {
 				case Ingredients.Water:
-					int given = waterController.GetMilliliters();
-					waterController.GameOver();
-					if(given >= answer - marge && given <= answer + marge) {
-						print ("Winner");
-					} else {
-						print ("Loser");
-					}
+					waterController.StartGame();
 					break;
 			}
-			// finish last ingredient + if there is next do next, etc....
-
+		} else {
+			winLoseText.text = "Einde van dit spel";
+			StartCoroutine (this.backToOverview());
 		}
+	}
+
+	public void CheckAssignment() {
+		switch(currentIngredient.ingredient) {
+			case Ingredients.Water:
+				int given = waterController.GetMilliliters();
+				waterController.StopGame();
+				if(given >= currentAnswer - marge && given <= currentAnswer + marge) {
+					winLoseText.text = "GOED!!";
+					// score points?
+				} else {
+					winLoseText.text = "FOUT!!";
+				}
+				break;
+		}
+		StartCoroutine (this.startNextGame ());
+		recipe.CheckFirst();
+	}
+
+	private IEnumerator startFirstGame() {
+		yield return new WaitForSeconds (1);
+		NextGame ();
+	}
+
+	private IEnumerator startNextGame() {
+		yield return new WaitForSeconds (3);
+		winLoseText.text = string.Empty;
+		NextGame ();
+	}
+
+	private IEnumerator backToOverview() {
+		yield return new WaitForSeconds (3);
+		Application.LoadLevel ("BakeryOverview");
 	}
 }
 
@@ -58,8 +96,8 @@ public class Recipe {
 		this.ingredients = new List<RecipeRow>();
 	}
 
-	public void AddRecipeRow(Ingredients ingredient, int amount, bool finished) {
-		this.ingredients.Add (new RecipeRow(ingredient, amount, finished));
+	public void AddRecipeRow(Ingredients ingredient, float amount, bool finished, UnitPrexixes unitPrefix, string unit) {
+		this.ingredients.Add (new RecipeRow(ingredient, amount, finished, unitPrefix, unit));
 	}
 
 	public RecipeRow GetNext() {
@@ -70,26 +108,57 @@ public class Recipe {
 		}
 		return null;
 	}
+
+	public void CheckFirst() {
+		RecipeRow ingredient = this.GetNext ();
+		ingredient.finished = true;
+	}
+
+	public override string ToString ()
+	{
+		string text = string.Format ("Recept voor {0} ({1}x) \n", this.product, this.amount);
+		foreach (RecipeRow r in this.ingredients) {
+			text += r.ToString() + "\n";
+		}
+		return text;
+	}
 }
 
 public class RecipeRow {
 	public bool finished { get; set; }
 	public Ingredients ingredient { get; private set; }
-	public int amount { get; private set; }
-	//eenheid?
+	public float amount { get; private set; }
+	public UnitPrexixes unitPrefix { get; private set; }
+	public string unit { get; private set; }
 
-	public RecipeRow(Ingredients ingredient, int amount, bool finished) {
+	public RecipeRow(Ingredients ingredient, float amount, bool finished, UnitPrexixes unitPrefix, string unit) {
 		this.ingredient = ingredient;
 		this.amount = amount;
 		this.finished = finished;
+		this.unitPrefix = unitPrefix;
+		this.unit = unit;
 	}
 
 	public override string ToString() {
-		return ingredient.ToString() + "           " + amount;
+		string text = string.Format ("{0} \t\t\t {1} {2}{3}", ingredient.ToString(), amount, unitPrefix.ToString(), unit);
+		if(finished) {
+			text += " (klaar)";
+		}
+		return text;
 	}
 }
 
 public enum Ingredients {
 	Bloem,
 	Water
+}
+
+public enum UnitPrexixes {
+	m = 1,
+	c = 10,
+	d = 100,
+	_ = 1000,
+	da = 10000,
+	h = 100000,
+	k = 1000000
 }
--- Game/Assets/Scripts/Workbench/WaterController.cs
+++ Game/Assets/Scripts/Workbench/WaterController.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System;
 
 public class WaterController : MonoBehaviour {
 	public Transform measuringCup;
+	public Text millilitersText;
+	// MeasuringCup is 1 [unitPrefix]l
+	public UnitPrexixes unitPrefix;
 
-	private bool gameOver;
+	private bool playing;
+	private float minPosX;
+	private float maxPosX;
+	private float smoothMove;
 	private bool increasing = false;
 	private bool decreasing = false;
 	private float maxHeight;
@@ -16,7 +23,10 @@ public class WaterController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.gameOver = false;
+		playing = false;
+		minPosX = 6.64f;
+		maxPosX = 14f;
+		smoothMove = 0.1f;
 		maxHeight = 2f;
 		minHeight = 0f;
 		minRotation = 0f;
@@ -26,49 +36,74 @@ public class WaterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!gameOver){
-			print (this.GetMilliliters ());
-			if(increasing) {
-				this.changeWaterLevelTo(maxHeight);
-			} else if (decreasing) {
-				if(measuringCup.localRotation.z < maxRotation) {
-					measuringCup.Rotate(0f, 0f, smoothRotate);
-				} else {
-					this.changeWaterLevelTo(minHeight);
+		if(playing){
+			//print (playing);
+			if(measuringCup.position.x > minPosX + smoothMove) {
+				this.MoveCupTo(minPosX);
+			} else {
+				millilitersText.text = this.GetMilliliters ().ToString();
+				if(increasing) {
+					this.ChangeWaterLevelTo(maxHeight);
+				} else if (decreasing) {
+					if(measuringCup.localRotation.z < maxRotation) {
+						measuringCup.Rotate(0f, 0f, smoothRotate);
+					} else {
+						this.ChangeWaterLevelTo(minHeight);
+					}
+				} else if (!decreasing) {
+					if(measuringCup.localRotation.z > minRotation) {
+						measuringCup.Rotate(0f, 0f, -smoothRotate);
+					} else {
+						measuringCup.rotation = Quaternion.identity;
+					}
 				}
-			} else if (!decreasing) {
-				if(measuringCup.localRotation.z > minRotation) {
-					measuringCup.Rotate(0f, 0f, -smoothRotate);
-				} else {
-					measuringCup.rotation = Quaternion.identity;
-				}
+			}
+		} else {
+			if(measuringCup.position.x < maxPosX - smoothMove) {
+				this.MoveCupTo(maxPosX);
+			} else {
+				Vector3 localScale = this.transform.localScale;
+				this.transform.localScale = new Vector3(localScale.x, 0f, localScale.z);
 			}
 		}
 	}
 
-	private void changeWaterLevelTo(float height) {
+	private void MoveCupTo(float posX) {
+		Vector3 pos = measuringCup.position;
+		measuringCup.position = new Vector3(Mathf.Lerp(pos.x, posX, smoothMove), pos.y, pos.z);
+	}
+
+	private void ChangeWaterLevelTo(float height) {
 		Vector3 localScale = this.transform.localScale;
 		this.transform.localScale = new Vector3(localScale.x, Mathf.Lerp (localScale.y, height, 0.005f), localScale.z);
 	}
 
 	public int GetMilliliters() {
-		return Convert.ToInt32(1000f * this.transform.localScale.y / 2f);
+		return Convert.ToInt32((int)unitPrefix * this.transform.localScale.y / 2f);
 	}
 
 	public void StartStopIncreasing() {
-		if(!decreasing && measuringCup.localRotation.z <= minRotation) {
+		if(!decreasing && measuringCup.localRotation.z <= minRotation && measuringCup.position.x < minPosX + smoothMove) {
 			increasing = !increasing;
 		}
 	}
 
 	public void StartStopDecreasing() {
-		if(!increasing) {
+		if(!increasing && measuringCup.position.x < minPosX + smoothMove) {
 			decreasing = !decreasing;
 		}
 	}
 
-	public void GameOver() {
-		// next Game
-		this.gameOver = true;
+	public void StartGame() {
+		if(!playing) {
+			this.playing = true;
+		}
+	}
+
+	public void StopGame() {
+		if(playing) {
+			this.playing = false;
+			millilitersText.text = string.Empty;
+		}
 	}
 }
