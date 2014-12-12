1491cb63c15235d5a59dec611110eb3a08d27bb8
bartbouten <bart.bouten@student.fontys.nl>
2014-12-12 16:48:03 +0100

    lahalsd

--- Game/Assets/Scripts/Counter/DragManager.cs
+++ Game/Assets/Scripts/Counter/DragManager.cs
@@ -3,34 +3,43 @@ using System.Collections;
 
 public class DragManager : MonoBehaviour {
 
+	private float dragSpeed;
+
 	// Use this for initialization
 	void Start () {
-	
+		dragSpeed = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+    }
+
+	void FixedUpdate() {
 		if (Input.touchCount == 1) {
 			Touch touch = Input.GetTouch(0);
 			GameObject hit = InputDetection.CheckTouch(touch.position);
 			Vector3 touchPosition = Vector3.zero;
-
-            if(hit != null && touch.phase == TouchPhase.Stationary) {
+			
+			if(hit != null && touch.phase == TouchPhase.Stationary) {
 				touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-				if (touchPosition.x < transform.position.x) {
-					transform.Translate(Vector3.left * 10 * Time.deltaTime);
-				}
-				else if (touchPosition.x > transform.position.x) {
-					transform.Translate(Vector3.right * 10 * Time.deltaTime);
-				}
-
-				if (touchPosition.y < transform.position.y) {
-					transform.Translate(Vector3.down * 10 * Time.deltaTime);
-				}
-				else if (touchPosition.y > transform.position.y) {
-					transform.Translate(Vector3.up * 10 * Time.deltaTime);
+				
+				if(touchPosition.x != transform.position.x || touchPosition.y != transform.position.y) {
+					if (touchPosition.x < transform.position.x) {
+						transform.Translate(Vector3.left * dragSpeed * Time.deltaTime);
+					}
+					else if (touchPosition.x > transform.position.x) {
+						transform.Translate(Vector3.right * dragSpeed * Time.deltaTime);
+					}
+					
+					if (touchPosition.y < transform.position.y) {
+						transform.Translate(Vector3.down * dragSpeed * Time.deltaTime);
+					}
+					else if (touchPosition.y > transform.position.y) {
+                        transform.Translate(Vector3.up * dragSpeed * Time.deltaTime);
+                    }
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 }
