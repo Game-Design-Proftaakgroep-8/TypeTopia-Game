9a15678c29743c684a53e4cb05806ed3beff6209
bartbouten <bart.bouten@student.fontys.nl>
2014-12-18 13:21:52 +0100

    Vanalles en nog wat

---
+++ Game/Assets/Scripts/Counter/CounterManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CounterManager : MonoBehaviour {
+
+	public GameObject hand;
+	private HandMovement handMovement;
+
+	private bool start;
+
+	// Use this for initialization
+	void Start () {
+		handMovement = hand.GetComponent<HandMovement> ();
+		start = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(start) {
+			handMovement.MoveIn();
+			start = false;
+		}
+
+	}
+}
--- Game/Assets/Scripts/Counter/DragManager.cs
+++ Game/Assets/Scripts/Counter/DragManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class DragManager : MonoBehaviour {
 
+	private GameObject lastTouched;
+
 	// Use this for initialization
 	void Start () {
-
+		lastTouched = null;
 	}
 	
 	// Update is called once per frame
@@ -13,15 +15,30 @@ public class DragManager : MonoBehaviour {
 		if (Input.touchCount == 1) {
 			Touch touch = Input.GetTouch(0);
 			GameObject hit = InputDetection.CheckTouch(touch.position);
+			lastTouched = hit;
 			Vector2 touchPosition = Vector2.zero;
 			
-			if(hit != null && touch.phase == TouchPhase.Moved) {
-				touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-				
-				if(touchPosition.x != transform.position.x || touchPosition.y != transform.position.y) {
-					transform.position = touchPosition;
-                }
+			if(hit != null) {
+				if(hit.tag == "money") {
+					touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+					
+					if(touchPosition.x != transform.position.x || touchPosition.y != transform.position.y) {
+						hit.transform.position = touchPosition;
+	                }
+				}
             }
         }
     }
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "hand") {
+			lastTouched.transform.SetParent(col.gameObject.transform);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if(col.tag == "hand") {
+			lastTouched.transform.SetParent(null);
+		}
+	}
 }
