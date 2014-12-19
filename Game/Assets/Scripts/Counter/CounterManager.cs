1986743ff8e5cdeff1b8d2387b190c967b5a5a22
bartbouten <bart.bouten@student.fontys.nl>
2014-12-19 16:57:20 +0100

    Vanalles en nog wat

--- Game/Assets/Scripts/Counter/CounterManager.cs
+++ Game/Assets/Scripts/Counter/CounterManager.cs
@@ -6,24 +6,50 @@ public class CounterManager : MonoBehaviour {
 	public GameObject hand;
 	private HandMovement handMovement;
 
-	private bool start;
+	public GameObject eenEuro;
+	public GameObject tweeEuro;
+	public GameObject vijfEuro;
+
 	private bool gameOver;
+
+	private float moneyCustomer;
+	private float demandCustomer;
+	private float priceBread;
+	private float cost;
+	private float answer;
+
 
 	// Use this for initialization
 	void Start () {
 		handMovement = hand.GetComponent<HandMovement> ();
-		start = true;
 		gameOver = false;
+
+		setQuestion ();
+		handMovement.MoveIn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(start) {
-			handMovement.MoveIn();
-			start = false;
-		}
 
 	}
+
+	private void setQuestion() {
+		priceBread = Random.Range (1, 10);
+		demandCustomer = Random.Range (1, 5);
+		cost = priceBread * demandCustomer;
+		moneyCustomer = Random.Range (cost + 2, 60);
+		answer = moneyCustomer - cost;
+
+		generateCustomerMoney ();
+	}
+
+	private void generateCustomerMoney() {
+
+		
+		if (moneyCustomer > 0)
+			generateCustomerMoney();
+	}
+
 
 	public void confirm() {
 		gameOver = true;
