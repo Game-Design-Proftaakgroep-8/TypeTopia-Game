2e0ad20d9daeec07334131d5a743acbfbcb057d6
bartbouten <bart.bouten@student.fontys.nl>
2015-01-07 13:17:17 +0100

    zomaar

--- Game/Assets/Scripts/Counter/CounterManager.cs
+++ Game/Assets/Scripts/Counter/CounterManager.cs
@@ -6,9 +6,9 @@ public class CounterManager : MonoBehaviour {
 	public GameObject hand;
 	private HandMovement handMovement;
 
-	public GameObject eenEuro;
-	public GameObject tweeEuro;
-	public GameObject vijfEuro;
+	public Object eenEuro;
+	public Object tweeEuro;
+	public Object vijfEuro;
 
 	private bool gameOver;
 
@@ -34,18 +34,58 @@ public class CounterManager : MonoBehaviour {
 	}
 
 	private void setQuestion() {
-		priceBread = Random.Range (1, 10);
-		demandCustomer = Random.Range (1, 5);
+		priceBread = Mathf.Round(Random.Range (1, 10));
+		demandCustomer = Mathf.Round(Random.Range (1, 5));
 		cost = priceBread * demandCustomer;
-		moneyCustomer = Random.Range (cost + 2, 60);
+		moneyCustomer = Mathf.Round(Random.Range (cost + 2, 60));
 		answer = moneyCustomer - cost;
+
+		print ("priceBread: " + priceBread);
+		print ("demandCustomer: " + demandCustomer);
+		print ("cost: " + cost);
+		print ("moneyCustomer: " + moneyCustomer);
 
 		generateCustomerMoney ();
 	}
 
 	private void generateCustomerMoney() {
+		int randomMoney = Random.Range (0, 3);
+		GameObject money = (GameObject)eenEuro;
+		Vector2 inHand = new Vector2(hand.transform.position.x, hand.transform.position.y);
 
-		
+		switch (randomMoney) {
+		case 0:
+			if(moneyCustomer < 1) {
+				break;
+			}
+			else {
+				money = (GameObject)Instantiate(eenEuro);
+				money.transform.position = inHand;
+				moneyCustomer -= 1;
+				break;
+			}
+		case 1:
+			if(moneyCustomer < 2) {
+				break;
+			}
+			else {
+				money = (GameObject)Instantiate(tweeEuro);
+				money.transform.position = inHand;
+				moneyCustomer -= 2;
+				break;
+			}
+		case 2:
+			if(moneyCustomer < 5) {
+				break;
+			}
+			else {
+				money = (GameObject)Instantiate(vijfEuro);
+				money.transform.position = inHand;
+				moneyCustomer -= 5;
+				break;
+			}
+		}
+
 		if (moneyCustomer > 0)
 			generateCustomerMoney();
 	}
