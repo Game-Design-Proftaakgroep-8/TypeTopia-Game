0463b389fb19d109cf420c38a1bd4d5eff3f6d4e
xChocoMel <melaniekwetters@hotmail.com>
2015-01-09 11:15:46 +0100

    database

--- Game/Assets/Scripts/Database.cs
+++ Game/Assets/Scripts/Database.cs
@@ -1,15 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Data;
+using MySql.Data.MySqlClient;
 
-public class Database : MonoBehaviour {
+public class Database {
 
-	// Use this for initialization
-	void Start () {
-	
+	//fields
+	private MySqlConnection conn;
+
+	public Database()
+	{
+		string connectionString =
+			"host=localhost;" +
+				"database=topiatrainer;" +
+				"username=root;" +
+				"password=IJsje!123;" +
+				"port=3306;";
+
+		conn = new MySqlConnection (connectionString);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void ConnectionBase(MySqlCommand command)
+	{
+		try
+		{
+			if (conn.State != ConnectionState.Open)
+			{
+				conn.Open();
+			}
+			command.ExecuteNonQuery();
+		}
+		catch
+		{
+			if (conn.State == ConnectionState.Open)
+			{
+				conn.Close();
+			}
+		}
+		finally
+		{
+			if (conn.State == ConnectionState.Open)
+			{
+				conn.Close();
+			}
+		}
+	}
+
+	public bool CheckLogin(string name, string password)
+	{
+		string SQL = "SELECT * FROM topiatrainer.Person WHERE pName=@nm AND pPassword=@pw";
+		MySqlCommand cmd = new MySqlCommand (SQL, conn);
+		cmd.Parameters.AddWithValue ("@nm", name);
+		cmd.Parameters.AddWithValue ("@pw", password);
+
+		bool exist = false;
+
+		try
+		{
+			if (conn.State == ConnectionState.Closed)
+			{
+				conn.Open();
+			}
+
+			MySqlDataReader reader = cmd.ExecuteReader ();
+
+			while (reader.Read())
+			{
+				exist = true;
+			}
+
+			reader.Close();
+		}
+		catch
+		{
+		}
+
+		conn.Close ();
+
+		return exist;
 	}
 }
