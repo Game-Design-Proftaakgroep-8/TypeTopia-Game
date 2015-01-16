db91b264f1b1379733f58d9f30e6e61d65449d96
Alexandervc <a.vancuijk@student.fontys.nl>
2015-01-16 14:57:23 +0100

    db + suminfo

--- Game/Assets/Scripts/Database/Database.cs
+++ Game/Assets/Scripts/Database/Database.cs
@@ -21,9 +21,28 @@ public class Database {
 		persons.Add (new Person ("alex", "123"));
 		persons.Add (new Person ("bart", "bb"));
 
-		scores.Add ("mel", 1100);
-		scores.Add ("alex", 1450);
-		scores.Add ("bart", 760);
+		scores.Add (new Score("mel", 1100));
+		scores.Add (new Score("alex", 1450));
+		scores.Add (new Score("bart", 760));
+
+		sums.Add (new SumInfo ("money", 0, 1, 10, 0));
+		sums.Add (new SumInfo ("money", 1, 0, 5, 1));
+		sums.Add (new SumInfo ("money", 2, 0, 6, 2));
+		sums [sums.Count - 1].AddCommaOption (0.25);
+		sums [sums.Count - 1].AddCommaOption (0.50);
+		sums [sums.Count - 1].AddCommaOption (0.75);
+		sums.Add (new SumInfo ("money", 3, 0, 6, 2));
+		sums.Add (new SumInfo ("time", 0, 0, 23, 0));
+		sums.Add (new SumInfo ("time", 1, 0, 23, 2));
+		sums [sums.Count - 1].AddCommaOption (0.15);
+		sums [sums.Count - 1].AddCommaOption (0.30);
+		sums [sums.Count - 1].AddCommaOption (0.45);
+		sums.Add (new SumInfo ("time", 2, 0, 23, 2));
+		sums.Add (new SumInfo ("time", 3, 0, 23, 2));
+		sums.Add (new SumInfo ("volume", 0, 1, 100, 0));
+		sums.Add (new SumInfo ("volume", 1, 1, 999, 0));
+		sums.Add (new SumInfo ("volume", 2, 0, 100, 1));
+		sums.Add (new SumInfo ("volume", 3, 0, 100, 1));
 	}
 //
 //	public void ConnectionBase(MySqlCommand command)
--- Game/Assets/Scripts/Database/SumInfo.cs
+++ Game/Assets/Scripts/Database/SumInfo.cs
@@ -4,21 +4,25 @@ using System.Collections.Generic;
 
 public class SumInfo
 {
-		public int minRange { get; private set; }
-		public int maxRange { get; private set; }
-		public int sumCommas { get; private set; }
-		public List<int> commaOptions { get; private set; }
-		
-		public SumInfo(int minRange, int maxRange, int sumCommas) 
+	public string sumType { get; private set; }
+	public int sumLevel { get; private set; }
+	public int minRange { get; private set; }
+	public int maxRange { get; private set; }
+	public int sumCommas { get; private set; }
+	public List<int> commaOptions { get; private set; }
+	
+	public SumInfo(string sumType, int sumLevel, int minRange, int maxRange, int sumCommas) 
 	{
-			this.minRange = minRange;
-			this.maxRange = maxRange;
-			this.sumCommas = sumCommas;
-			this.commaOptions = new List<int>();
-		}
-		
-		public void AddCommaOption(int commaOption) 
+		this.sumType = sumType;
+		this.sumLevel = sumLevel;
+		this.minRange = minRange;
+		this.maxRange = maxRange;
+		this.sumCommas = sumCommas;
+		this.commaOptions = new List<int>();
+	}
+	
+	public void AddCommaOption(int commaOption) 
 	{
-			this.commaOptions.Add(commaOption);
-		}
+		this.commaOptions.Add(commaOption);
+	}
 }
