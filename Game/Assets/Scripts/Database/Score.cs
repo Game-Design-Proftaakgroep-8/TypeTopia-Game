ba318b91960897cdc413f069810613f2804b7a3c
Alexandervc <a.vancuijk@student.fontys.nl>
2015-01-16 15:10:36 +0100

    db

--- Game/Assets/Scripts/Database/Score.cs
+++ Game/Assets/Scripts/Database/Score.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Score
+public class Score : IComparable<Score>
 {
 	public string name {get; private set;}
 	public int score {get; private set;}
@@ -10,5 +11,9 @@ public class Score
 	{
 		this.name = name;
 		this.score = score;
+	}
+
+	public int CompareTo(Score other) {
+		return this.score.CompareTo (other.score);
 	}
 }
